a097fd75a7ba51fd69c96c5a6dc750622d7b08e9
fumi <you@example.com>
2022-02-11 05:00:29 -0800

    jpegrcvに間違えて書いていたので_errorに移した

--- rcvudp/jpegrvc_error.c
+++ rcvudp/jpegrvc_error.c
@@ -7,6 +7,10 @@
 #include <time.h>
 #include <unistd.h>
 
+#define UDP_SIZE 1472
+#define UDP_HEADER 8
+#define UDP_DATASIZE (UDP_SIZE - UDP_HEADER)
+
 void print_diff_time(struct timeval start_time, struct timeval end_time) {
     struct timeval diff_time;
     if (end_time.tv_usec < start_time.tv_usec) {
@@ -35,46 +39,74 @@ int main() {
     int flg = 1;
     uint8_t local_id = 0;
     uint32_t global_id = 0, size = 0;
-
+    uint32_t global_counter = 0;
     memset(buf, 0, sizeof(buf));
     memset(framebuf, 0, sizeof(framebuf));
     uint32_t *buf_32 = (uint32_t *)buf;
     uint32_t *framebuf_32 = (uint32_t *)framebuf;
     // フレームループ
-    for (int k = 0; k < 10; k++) {
+    int wari = 0, amari = 0;
+    for (int k = 0; k < 5; k++) {
         // 1280*8*90ループ
         for (int j = 0; j < 90; j++) {
             int bufcounter = 0;
             // 1280*8の画像ループ
+            int broken = 0, internal_counter = 0;
             while (flg) {
                 int received = recv(sock, buf, sizeof(buf), 0) - 8;
                 global_id = buf_32[0];
                 size = buf_32[1] & 0x00FFFFFF;
                 local_id = (buf_32[1] >> 24) & 0xFF;
                 printf(
-                    "received:%d global_id:%u size:%u local:%u bufcounter:%d\n",
-                    received, global_id, size, local_id, bufcounter);
-                gettimeofday(&start_time, NULL);
+                    "received:%d global_id:%u size:%u local:%u bufcounter:%d "
+                    "intcnt:%d\n",
+                    received, global_id, size, local_id, bufcounter,
+                    internal_counter);
+                wari = size / UDP_DATASIZE;
+                amari = size - (wari * UDP_DATASIZE);  // amari = size%UDP_DATA;
+                if (local_id < wari) {  // 1464 //足りなかったら壊れてる
+                    broken = received != UDP_DATASIZE ? 1 : 0;
+                } else {
+                    broken = received != amari ? 1 : 0;
+                }
+
+                if (local_id != internal_counter) {
+                    // 内部カウンタとlocal_idが違ったらパケット不着なので壊れてる
+                    broken = 1;
+                    printf("broken: global_id=%d local_id=%d internal=%d\n",
+                           global_id, local_id, internal_counter);
+                }
+
                 for (int i = 0; i < received / 4; i++) {
                     framebuf_32[i + (bufcounter / 4)] = htonl(buf_32[i + 2]);
                 }
-                gettimeofday(&end_time, NULL);
-                // memcpy(framebuf, buf+bufcounter+8, received);
-                bufcounter += received;
-                if (local_id ==
-                    (size /
-                     1464)) {  // 1464でなければなのでピッタリだとだめじゃん
+                if (broken) {
                     flg = 0;
-                    printf("nuke\n");
                 }
+                if (internal_counter >= wari) {  // amariまで完了
+                    flg = 0;
+                }
+                if (amari == 0 && (internal_counter == (wari - 1))) {
+                    // amariが0だったとき、wari-1まで終わったらOK
+                    flg = 0;
+                }
+                bufcounter += received;
+                internal_counter++;
             }
 
-            char moji[32];
-            sprintf(moji, "binout/%04d.bin", global_id);
-            FILE *fp = fopen(moji, "wb");
-            int n = fwrite(framebuf, sizeof(char), bufcounter, fp);
-            fclose(fp);
+            if (global_counter != global_id) {
+            }
+
+            if (!broken) {
+                char moji[32];
+                sprintf(moji, "binout/%04d.bin", global_id);
+                FILE *fp = fopen(moji, "wb");
+                int n = fwrite(framebuf, sizeof(char), bufcounter, fp);
+                fclose(fp);
+            } else {
+            }
             flg = 1;
+            global_counter++;
         }
     }
 
